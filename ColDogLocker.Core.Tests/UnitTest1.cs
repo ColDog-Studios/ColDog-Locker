4f5213e4010c64ab24e463659eb2f88439000e56
Collin Laney <95191733+ColDog5044@users.noreply.github.com>
2025-12-30 09:55:44 -0500

    Migration Structure

---
+++ ColDogLocker.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ColDogLocker.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ ColDogLocker.Infrastructure/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace ColDogLocker.Infrastructure;
+
+public class Class1
+{
+
+}
