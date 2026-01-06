9eb2e8245d48e48842e41ffa6f8c5ca12f422d6f
Collin Laney <95191733+ColDog5044@users.noreply.github.com>
2026-01-06 09:50:02 -0500

    Update Namespace to match

--- ColDogLocker.Gui.WinUI/App.xaml.cs
+++ ColDogLocker.Gui.WinUI/App.xaml.cs
@@ -19,7 +19,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace ColDogLocker.Gui.WinUI
+namespace ColDogStudios.ColDogLocker.Gui.WinUI
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
