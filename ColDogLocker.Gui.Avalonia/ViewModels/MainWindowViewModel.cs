da9a841681c89ea47ca54cadfcc7dc584d082810
Collin Laney <95191733+ColDog5044@users.noreply.github.com>
2026-05-20 19:36:30 -0400

    Add Avalonia MVVM project for future use

---
+++ ColDogLocker.Gui.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ColDogLocker.Gui.Avalonia.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ ColDogLocker.Gui.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ColDogLocker.Gui.Avalonia.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
