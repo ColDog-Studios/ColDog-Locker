594398df42a7395a2416fd1e0b9367f4ebaec1af
Collin Laney <95191733+ColDog5044@users.noreply.github.com>
2026-05-23 11:32:15 -0400

    chore: rename avalonia namespace to ColDogStudios.ColDogLocker.Avalonia

---
+++ ColDogLocker.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ColDogStudios.ColDogLocker.Avalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
--- ColDogLocker.Gui.Avalonia/ViewModels/MainWindowViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ColDogLocker.Gui.Avalonia.ViewModels
-{
-    public partial class MainWindowViewModel : ViewModelBase
-    {
-        public string Greeting { get; } = "Welcome to Avalonia!";
-    }
-}
