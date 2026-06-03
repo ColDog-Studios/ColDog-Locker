9f7e4847f1c8167cb3356867cb29e96b147621ab
Collin Laney <95191733+ColDog5044@users.noreply.github.com>
2026-06-03 11:35:57 -0400

    revert 627a7af
    Signed-off-by: Collin Laney <95191733+ColDog5044@users.noreply.github.com>

--- ColDogLocker.Avalonia/Views/Dialogs/NewLockerDialog.cs
+++ ColDogLocker.Avalonia/Views/Dialogs/NewLockerDialog.cs
@@ -189,23 +189,7 @@ namespace ColDogStudios.ColDogLocker.Avalonia.Views.Dialogs
                 Directory.CreateDirectory(defaultBasePath);
                 return defaultBasePath;
             }
-            catch (UnauthorizedAccessException)
-            {
-                return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            }
-            catch (IOException)
-            {
-                return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            }
-            catch (ArgumentException)
-            {
-                return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            }
-            catch (NotSupportedException)
-            {
-                return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            }
-            catch (PathTooLongException)
+            catch
             {
                 return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             }
